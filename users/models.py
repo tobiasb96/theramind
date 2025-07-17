ba817f98ba6bdea43df0fc2d886c871465066e4b
tobiasblanck <tobias@syntinels.com>
2025-07-17 20:15:55 +0200

    Update REQUIRED_FIELDS in User model to simplify user creation
    - Removed the first and last name fields from the REQUIRED_FIELDS list in the User model, streamlining the user creation process. This change enhances flexibility for user registration while maintaining the email as the primary identifier.

--- users/models.py
+++ users/models.py
@@ -81,7 +81,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     # The `USERNAME_FIELD` property tells us which field we will use to log in.
     # In this case we want it to be the email field.
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["first_name", "last_name"]
+    REQUIRED_FIELDS = []
 
     objects = UserManager()
 
