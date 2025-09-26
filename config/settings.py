8660151417e3d05dfbed0bdebb2c032f11ecc34b
Mathis1993 <m_eric01@uni-muenster.de>
2025-09-26 17:52:23 +0200

    Use static directory as input dir for static collection

--- config/settings.py
+++ config/settings.py
@@ -140,10 +140,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / "assets"]
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 
 # Media files
